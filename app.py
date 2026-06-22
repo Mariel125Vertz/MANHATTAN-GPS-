c68ebb0208ee183536039e93f197c2adbf2ddc90
Mariel125Vertz <marielvertz@gmail.com>
2026-06-22 00:45:58 -0600

    Corregir tipos MIME y orden de scripts

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
 from flask import Flask
+import mimetypes # Importa aquí arriba
 
 from configuracion import Configuracion
 from routes import registrar_rutas
 
-
 def crear_aplicacion(configuracion_prueba=None):
+    # Definimos los tipos MIME antes de crear la instancia de Flask
+    mimetypes.add_type('application/javascript', '.js')
+    mimetypes.add_type('text/css', '.css')
+    
     aplicacion = Flask(
         __name__,
         template_folder="templates",
         static_folder="static",
     )
+    
     aplicacion.config.from_object(Configuracion)
 
     if configuracion_prueba:
@@ -18,9 +23,7 @@ def crear_aplicacion(configuracion_prueba=None):
     registrar_rutas(aplicacion)
     return aplicacion
 
-
 aplicacion = crear_aplicacion()
-
 
 if __name__ == "__main__":
     aplicacion.run(
